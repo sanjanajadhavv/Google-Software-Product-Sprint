60a19d82c246a0e94b15bb26dcdb75252a786d52
Sanjana Jadhav <sxj190054@utdallas.edu>
2022-06-22 07:09:18 +0000

    translation functionality added

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -44,3 +44,22 @@ async function getStats() {
     statsListElement.innerText = stat;
 }
 
+function requestTranslation() {
+    const text = document.getElementById('text').value;
+    const languageCode = document.getElementById('language').value;
+
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerText = 'Loading...';
+
+    const params = new URLSearchParams();
+    params.append('text', text);
+    params.append('languageCode', languageCode);
+
+    fetch('/translate', {
+        method: 'POST',
+        body: params
+    }).then(response => response.text())
+    .then((translatedMessage) => {
+    resultContainer.innerText = translatedMessage;
+    });
+}
