56b8349d76fd32c72b7e1019f9146a4e4a55977b
Sanjana Jadhav <sxj190054@utdallas.edu>
2022-06-22 02:43:09 +0000

    update form

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -9,16 +9,48 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+        String finalText = "";
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+        // Get the value entered in the form.
+        String textValue = request.getParameter("text-input");
+        boolean phone = Boolean.parseBoolean(request.getParameter("phone"));
+        boolean linkedin = Boolean.parseBoolean(request.getParameter("linkedin"));
+        boolean handshake = Boolean.parseBoolean(request.getParameter("handshake"));
+        boolean email = Boolean.parseBoolean(request.getParameter("email"));
+        boolean other = Boolean.parseBoolean(request.getParameter("other"));
 
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
-  }
+        if (phone == true) {
+            finalText = "Phone Number: " + phone;
+        } else if (linkedin == true) {
+            finalText = "Linkedin: " + linkedin;
+        } else if (handshake == true) {
+            finalText = "Handshake: " + handshake;
+        } else if (email == true) {
+            finalText = "Email: " + email;
+        }
+        else if (other == true) {
+            finalText = "Other: " + email;
+        }
+
+        // Respond with the result.
+        response.setContentType("text/html;");
+        response.getWriter().println("You submitted: " + textValue);
+        System.out.println("Submission: " + finalText);
+
+    }
+
+    /**
+     * @return the request parameter, or the default value if the parameter
+     *         was not specified by the client
+     */
+    private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+        String value = request.getParameter(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
 }
